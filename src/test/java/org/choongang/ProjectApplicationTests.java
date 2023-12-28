d9c44016223f29f82bc817023894eada153105dd
easy-0 <lc_dyna@naver.com>
2023-12-28 14:06:21 +0900

    setting

--- src/test/java/org/choongang/ProjectApplicationTests.java
+++ src/test/java/org/choongang/ProjectApplicationTests.java
@@ -2,12 +2,15 @@ package org.choongang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.profiles.active=test")
 class ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
