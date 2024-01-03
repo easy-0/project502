aeb3da26f9f8d9fb772decaa09733a095c3a3d6d
easy-0 <lc_dyna@naver.com>
2024-01-03 12:29:09 +0900

    회원가입(인증, 인가)

--- src/main/java/org/choongang/commons/interceptors/CommonInterceptor.java
+++ src/main/java/org/choongang/commons/interceptors/CommonInterceptor.java
@@ -3,6 +3,7 @@ package org.choongang.commons.interceptors;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.choongang.member.MemberUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -13,7 +14,7 @@ public class CommonInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         checkDevice(request);
-
+        clearLoginData(request);
         return true;
     }
 
@@ -33,5 +34,17 @@ public class CommonInterceptor implements HandlerInterceptor {
 
         HttpSession session = request.getSession();
         session.setAttribute("device", device);
+    }
+
+    /**
+     * 경로에 "/member/login"이 있으면 세션을 비워서
+     * @param request
+     */
+    private void clearLoginData(HttpServletRequest request) {
+        String URL = request.getRequestURI();
+        if (URL.indexOf("/member/login") == -1) {
+            HttpSession session = request.getSession();
+            MemberUtil.clearLoginData(session);
+        }
     }
 }
